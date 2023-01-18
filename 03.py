b4b49363f27937d8a9aa21860555b352e71eca9d
Snezhana <tsvetkovasnezhana555@yandex.ru>
2023-01-18 13:54:16 +0300

    third commit

--- 03.py
+++ 03.py
@@ -12,10 +12,15 @@ class MiracleList:
         print(self.mass)
 
     def sort(self):
-        for i in range(1, len(self.mass)):
-            for j in range(i, len(self.mass)):
-                if self.mass[j - 1] > self.mass[j]:
-                    self.mass[j - 1], self.mass[j] = self.mass[j], self.mass[j - 1]
+        for i in range(len(self.mass) - 1):
+            flag_ = True
+            for j in range(len(self.mass) - i - 1):
+                if self.mass[j] > self.mass[j + 1]:
+                    self.mass[j], self.mass[j + 1] = self.mass[j + 1], self.mass[j]
+                    flag_ = False
+            if flag_:
+                break
+
         return self.mass
 
     def bin(self, number):
@@ -34,7 +39,7 @@ class MiracleList:
         else:
             print('Такого числа нет в массиве')
 
-list_ = MiracleList([2, 10, 8, 5, 6, 12, 13])
+list_ = MiracleList([2, 10, 8, 5, 6, 12, 13,3])
 list_.print()
 print('Отсортированный массив', list_.sort())
 list_.bin(12)
